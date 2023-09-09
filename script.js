d9397ae67cb2e10f8711e53480f29ef3d06817fe
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-09 10:06:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let fnc = '';
+	for (let i = 0; i < bound; i++) {
+		if(str.length == 0){
+			console.log(null);
+		}
+	}
+		
+	for (let i of str) {
+	    if (str.split(i).length - 1 === 1) {
+	        fnc = i;
+	        break;
+	    } else {
+	        index += 1;
+	    }
+	}
+	if (index === str.length-1) {
+	    console.log(null);
+	} else {
+	    console.log(fnc);
+	} 
 }
-const input = prompt("Enter a string");
+const input = prompt("Enter a string ");
 alert(firstNonRepeatedChar(input)); 
